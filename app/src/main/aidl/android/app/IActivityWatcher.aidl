ba114d923fd09ecfe4fab6f587c7d9f79ba90c01
Leizheng <leizheng@xiaomi.com>
2022-01-18 14:47:39 +0800

    Update appcompat version.

--- app/src/main/aidl/android/app/IActivityWatcher.aidl
+++
@@ -1,9 +0,0 @@
-package android.app;
-/**
- * Callback interface to watch the user's traversal through activities.
- * {@hide}
- */
-oneway interface IActivityWatcher {
-    void activityResuming(int activityId);
-    void closingSystemDialogs(String reason);
-}
--- app/src/main/java/com/genlz/jetpacks/service/ProcessMonitorService.kt
+++ app/src/main/java/com/genlz/jetpacks/service/ProcessMonitorService.kt
@@ -5,21 +5,29 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import androidx.annotation.RequiresPermission
+import com.genlz.jetpacks.di.ApplicationScope
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ProcessMonitorService : Service() {
+
+    @Inject
+    @ApplicationScope
+    internal lateinit var scope: CoroutineScope
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     @RequiresPermission("android.permission.SET_ACTIVITY_WATCHER")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        CoroutineScope(Dispatchers.Main).launch {
+        scope.launch {
             ProcessMonitorManager.getInstance(applicationContext).topInfoFlow().collectLatest {
-                val topTask = it.second
+                val (topProcess, topTask) = it
                 Log.d(TAG, "onStartCommand: ${topTask.topActivity}")
+                Log.d(TAG, "onStartCommand: ${topProcess.processName}")
             }
         }
         return START_STICKY_COMPATIBILITY
