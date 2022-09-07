035ebb001c8e7ba3e0e6647c2c4b47932c0719e8
leizheng <leizheng3@xiaomi.com>
2022-09-07 14:51:21 +0800

    Bump up some libs.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
-const val HILT = "2.42"
-const val NAVIGATION = "2.5.0-rc01"
+const val HILT = "2.43.2"
+const val NAVIGATION = "2.5.1"
 const val AGP = "7.2.1" //upgrade the dependency of gradle api simultaneously
 const val min_sdk = 21
 const val target_sdk = 32
@@ -9,11 +9,11 @@ const val KSP = "$KOTLIN-1.0.6"
 const val DOKKA = "1.6.21"
 
 const val coreKtx = "1.8.0"
-const val lifecycle = "2.5.0-rc01"
-const val coroutines = "1.6.0"
-const val appcompat = "1.6.0-alpha04"
-const val activity = "1.6.0-alpha04"
-const val material = "1.5.0"
+const val lifecycle = "2.5.1"
+const val coroutines = "1.6.4"
+const val appcompat = "1.6.0-beta01"
+const val activity = "1.6.0-rc01"
+const val material = "1.6.0"
 const val room = "2.4.2" //kapt for Apple silicon since 2.4.0-beta01.
 const val startup = "1.2.0-alpha01"
 const val datastore = "1.0.0"
@@ -25,15 +25,15 @@ const val work = "2.7.1" //for targeting S+ since 2.7.0
 const val browser = "1.4.0"
 const val webkit = "1.4.0"
 
-const val window = "1.1.0-alpha02"
-const val splash = "1.0.0-rc01"
+const val window = "1.1.0-alpha03"
+const val splash = "1.0.0"
 
 //Third party
 const val retrofit = "2.9.0"
 const val okhttp = "4.9.3"
-const val coil = "1.4.0"
+const val coil = "2.2.0"
 const val photoView = "2.3.0"
-const val gson = "2.8.9"
+const val gson = "2.9.1"
 const val libsu = "3.1.2"
 const val asm = "9.3"
 const val guava = "31.1-android"
